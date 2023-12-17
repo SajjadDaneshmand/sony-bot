371532e0cb41a974de46e413f4ae2f99f9c45864
Sajjad Daneshmand <Mr.SajjadDaneshmand@gmail.com>
2023-12-17 15:38:14 +0330

    tools are here

---
+++ src/tools.py
@@ -0,0 +1,12 @@
+import re
+
+
+def append_file(file_path, text):
+    with open(file_path, 'a') as f:
+        text += '\n'
+        return f.write(text)
+
+
+def is_valid_ip(ip):
+    ip_pattern = r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    return bool(re.match(ip_pattern, ip))
